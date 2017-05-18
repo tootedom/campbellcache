c41300c30962f5045e21861e61ebba2f4b995330
dominictootell <dominic.tootell@gmail.com>
2017-05-18 08:35:29 +0100

    hacking around with Single

--- index.js
+++ index.js
@@ -3,18 +3,33 @@
  */
 
 var HerdCache = require("./lib/herdcache");
+var CacheItem = require('./lib/cacheitem');
 
 
 var HerdCacheClient = new HerdCache({
     autodiscovery:true
 })
 
+var item = new CacheItem("string",null,false);
+
+
+console.log(item);
+
+item.key = "lkjlkj";
+
+
+
 
 console.log(HerdCacheClient.config);
 
 
+var single = HerdCacheClient.get('val');
+console.log("troublelelkjlkjlkj" + single);
+single.subscribe((item) => console.log("tiem: " + item));
 
-
+setTimeout(() => {
+    single.subscribe((item) => console.log("tiem: " + item));
+},1000);
 process.on( 'SIGINT', function() {
   console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
   // some other closing procedures go here
