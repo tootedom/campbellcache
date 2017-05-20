f91bda7032bd65d8fc7450552b3d1e8ea4ebbb02
dominictootell <dominic.tootell@gmail.com>
2017-05-20 14:51:09 +0100

    mucking around with cache

--- index.js
+++ index.js
@@ -23,13 +23,20 @@ item.key = "lkjlkj";
 console.log(HerdCacheClient.config);
 
 
-var single = HerdCacheClient.get('val');
+var single = HerdCacheClient.get('mykey');
 console.log("troublelelkjlkjlkj" + single);
-single.subscribe((item) => console.log("tiem: " + item));
-
+single.subscribe((item) => console.log("ITEM: " + item),null,()=>console.log("complete1"));
+single.subscribe((item) => console.log("ITEM2: " + item),null,()=>console.log("complete2"));
+single.subscribe((item) => console.log("ITEM3: " + item),null,()=>console.log("complete3"));
+single.subscribe((item) => console.log("ITEM4: " + item),null,()=>console.log("complete4"));
+console.log("after subscribe init")
 setTimeout(() => {
-    single.subscribe((item) => console.log("tiem: " + item));
-},1000);
+    single.subscribe((item) => console.log("tiem: " + item)
+    ,
+    null,
+    (item) => console.log("complete : " + item)
+    );
+},5000);
 process.on( 'SIGINT', function() {
   console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
   // some other closing procedures go here
