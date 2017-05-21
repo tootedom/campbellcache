3da325b282497ff8a4ed8448cd996f773ce31eb3
dominictootell <dominic.tootell@gmail.com>
2017-05-21 23:08:58 +0100

    messing with mock memcached

---
+++ test/observablememcached.test.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+
+var proxyquire = require('proxyquire');
+var memcachedMock = require('memcached-mock');
+
+describe('ObservableMemcached', function() {
+// module.exports.testSomething = function(done) {
+  var cached = proxyquire('../lib/observable-memcached', {memcached: memcachedMock});
+  // var mockmemcached = new memcachedMock('127.0.0.1:11211');
+  // cached.client.set("key","ljlkj",1000,function(err) {
+  //   if (!err) {
+  //     mockmemcached.get("key", function(err, data) {
+  //       console.log(data); // prints: world!
+  //     });
+  //   }
+// });
+
+  var cache = new cached(true);
+  console.log(cache);
+  cache.client.set("key","kkkkkk",1000,function() {});
+cache.client.get("key", function(err, data) {
+        console.log(data); // prints: world!
+      });
+  describe("Gets", function() {
+    it("Should return an observable from get request",function(done) {
+      var obs = cache.get("key");
+
+      obs.subscribe(function(value) {
+        console.log(value);
+        done();
+      });
+    })
+  })
+  // };
+});
