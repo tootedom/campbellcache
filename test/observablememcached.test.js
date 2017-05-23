0606e298d29cc0029e0ef40536f12b19020d9bab
dominictootell <dominic.tootell@gmail.com>
2017-05-23 22:32:17 +0100

    use CacheItem

--- test/observablememcached.test.js
+++ test/observablememcached.test.js
@@ -28,21 +28,27 @@ describe('ObservableMemcached', function() {
         monkeyPatchGet(1000,memcachedMock);
         this.timeout(5000);
         var obs = cache.get("key");
+
         var observerCount = 0;
         setTimeout(() => {
           obs.subscribe(function(value) {
-            assert.equal("BOB",value);
+            assert.equal("BOB",value.value());
             observerCount += 1;
           });
         },500);
 
         obs.subscribe(function(value) {
-          assert.equal("BOB",value);
+          assert.equal("BOB",value.value());
           observerCount += 1;
         });
 
         obs.subscribe(function(value) {
-          assert.equal("BOB",value);
+          assert.equal("BOB",value.value());
+          assert.equal("key",value.getKey());
+          assert.equal("key",value.key);
+          assert.equal(true,value.isFromCache());
+          assert.equal(true,value.hasValue());
+          assert.equal(false,value.isEmpty());
           observerCount += 1;
         });
 
@@ -50,8 +56,27 @@ describe('ObservableMemcached', function() {
           assert.equal(3,observerCount);
           done();
         },1500);
-      });
-  })
+    });
+
+    it("Returns observable from get request, with empty value, that takes time to fulfil",
+      function(done) {
+        // add a delay to the test
+        monkeyPatchGet(1000,memcachedMock);
+        this.timeout(5000);
+        var obs = cache.get("does_not_exist");
+
+        obs.subscribe(function(value) {
+          assert.equal(null,value.value());
+          assert.equal("BOB",value.value("BOB"));
+          assert.equal("does_not_exist",value.getKey());
+          assert.equal("does_not_exist",value.key);
+          assert.equal(false,value.isFromCache());
+          assert.equal(false,value.hasValue());
+          assert.equal(true,value.isEmpty());
+          done();
+        });
+    });
+  });
 });
 
 function monkeyPatchGet(timeout,mock) {
