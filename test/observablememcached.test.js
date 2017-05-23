8d281edebb579c760c610887b7fe20213ae07dc9
dominictootell <dominic.tootell@gmail.com>
2017-05-23 20:56:40 +0100

    add shutdown

--- test/observablememcached.test.js
+++ test/observablememcached.test.js
@@ -1,44 +1,65 @@
 var assert = require('assert');
 
 var proxyquire = require('proxyquire');
-var memcachedMock = require('memcached-mock');
+//var memcachedMock = require('memcached-mock');
 
 describe('ObservableMemcached', function() {
-// module.exports.testSomething = function(done) {
-  var cached = proxyquire('../lib/observable-memcached', {memcached: memcachedMock});
-  // var mockmemcached = new memcachedMock('127.0.0.1:11211');
-  // cached.client.set("key","ljlkj",1000,function(err) {
-  //   if (!err) {
-  //     mockmemcached.get("key", function(err, data) {
-  //       console.log(data); // prints: world!
-  //     });
-  //   }
-// });
+  var memcachedMock;
+  var cached;
+  var cache;
 
-  var cache = new cached(true);
-  console.log(cache);
-  cache.client.set("key","kkkkkk",1000,function() {});
-  cache.client.get("key", function(err, data) {
-        console.log(data); // prints: world!
+  beforeEach(function() {
+    memcachedMock = require('memcached-mock');
+    cached = proxyquire('../lib/observable-memcached', {memcached: memcachedMock});
+    cache = new cached(true);
+    // Set key to BOB for 10 mins
+    cache.client.set("key","BOB",600,function() {});
   });
 
-  const originalGet = memcachedMock.prototype.get;
+  afterEach(function() {
+    console.log("Shutting down");
+    cache.shutdown();
+  });
+
+  describe("Gets", function() {
+    it("Returns observable from get request that takes time to fulfil",
+      function(done) {
+        // add a delay to the test
+        monkeyPatchGet(1000,memcachedMock);
+        this.timeout(5000);
+        var obs = cache.get("key");
+        var observerCount = 0;
+        setTimeout(() => {
+          obs.subscribe(function(value) {
+            assert.equal("BOB",value);
+            observerCount += 1;
+          });
+        },500);
+
+        obs.subscribe(function(value) {
+          assert.equal("BOB",value);
+          observerCount += 1;
+        });
+
+        obs.subscribe(function(value) {
+          assert.equal("BOB",value);
+          observerCount += 1;
+        });
+
+        setTimeout(() => {
+          assert.equal(3,observerCount);
+          done();
+        },1500);
+      });
+  })
+});
+
+function monkeyPatchGet(timeout,mock) {
+  const originalGet = mock.prototype.get;
   const get = function(key,cb) {
     setTimeout(() => {
       originalGet.call(this,key,cb);
     },1000);
   }
-  memcachedMock.prototype.get = get
-
-  describe("Gets", function() {
-    it("Should return an observable from get request",function(done) {
-      var obs = cache.get("key");
-
-      obs.subscribe(function(value) {
-        console.log(value);
-        done();
-      });
-    })
-  })
-  // };
-});
+  mock.prototype.get = get
+}
