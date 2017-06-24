e4eacf6cde93537af7d61605b5d9007df5095a86
dominictootell <dominic.tootell@gmail.com>
2017-06-24 15:28:35 +0100

    fix index

--- index.js
+++ index.js
@@ -2,52 +2,54 @@
  * Module dependencies
  */
 
-var HerdCache = require("./lib/herdcache");
-var CacheItem = require('./lib/cacheitem');
+module.exports = require("./lib/herdcache.js")
+
+// var HerdCache = require("./lib/herdcache");
+// var CacheItem = require('./lib/cacheitem');
 
 
-var HerdCacheClient = new HerdCache({
-    autodiscovery:true
-})
+// var HerdCacheClient = new HerdCache({
+//     autodiscovery:true
+// })
 
-var item = new CacheItem("string",null,false);
-
-
-console.log(item);
-
-item.key = "lkjlkj";
+// var item = new CacheItem("string",null,false);
 
 
+// console.log(item);
+
+// item.key = "lkjlkj";
 
 
-console.log(HerdCacheClient.config);
 
 
-var single = HerdCacheClient.get('mykey');
-console.log("Got Object" + single);
-single.subscribe((item) => console.log("ITEM: " + item),null,()=>console.log("complete1"));
-single.subscribe((item) => console.log("ITEM2: " + item),null,()=>console.log("complete2"));
-single.subscribe((item) => console.log("ITEM3: " + item),null,()=>console.log("complete3"));
-single.subscribe((item) => console.log("ITEM4: " + item),null,()=>console.log("complete4"));
-console.log("After Subs" + single);
+// console.log(HerdCacheClient.config);
 
-setTimeout(() => {
-    var single2 = HerdCacheClient.get('mykey');
-    single2.subscribe((item) => console.log("ITEM5: " + item),null,()=>console.log("complete5"));
-},1000);
 
-console.log("after subscribe init")
-setTimeout(() => {
-    single.subscribe((item) => console.log("tiem: " + item)
-    ,
-    null,
-    (item) => console.log("complete : " + item)
-    );
-},5000);
-process.on( 'SIGINT', function() {
-  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
-  // some other closing procedures go here
-  process.exit( );
-})
+// var single = HerdCacheClient.get('mykey');
+// console.log("Got Object" + single);
+// single.subscribe((item) => console.log("ITEM: " + item),null,()=>console.log("complete1"));
+// single.subscribe((item) => console.log("ITEM2: " + item),null,()=>console.log("complete2"));
+// single.subscribe((item) => console.log("ITEM3: " + item),null,()=>console.log("complete3"));
+// single.subscribe((item) => console.log("ITEM4: " + item),null,()=>console.log("complete4"));
+// console.log("After Subs" + single);
+
+// setTimeout(() => {
+//     var single2 = HerdCacheClient.get('mykey');
+//     single2.subscribe((item) => console.log("ITEM5: " + item),null,()=>console.log("complete5"));
+// },1000);
+
+// console.log("after subscribe init")
+// setTimeout(() => {
+//     single.subscribe((item) => console.log("tiem: " + item)
+//     ,
+//     null,
+//     (item) => console.log("complete : " + item)
+//     );
+// },5000);
+// process.on( 'SIGINT', function() {
+//   console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+//   // some other closing procedures go here
+//   process.exit( );
+// })
 
 
